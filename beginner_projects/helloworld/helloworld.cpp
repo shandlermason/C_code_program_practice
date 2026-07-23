7a3ad489b9748f44c6f896612f87f356b0a1837b
Shandler Mason <samason4@ncsu.edu>
2026-07-23 15:18:22 -0400

    updated directory name and read me

---
+++ beginner_projects/helloworld/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Type and name of function
+int main() { // Beginning of function
+
+    //What the function does
+    cout << "Hello world!" << endl;
+    cout << "Enjoy yourself with C++!"  << endl;
+
+    return 0;
+
+} // End of function
